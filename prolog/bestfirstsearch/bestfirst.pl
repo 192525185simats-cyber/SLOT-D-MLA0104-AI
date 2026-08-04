37429660bda8a44818f397380db86b5e74ac543a
Shaik roshni <192525185.simats@saveetha.com>
2026-08-04 14:14:25 +0530

    Add files via upload

---
+++ prolog/bestfirstsearch/bestfirst.pl
@@ -0,0 +1,15 @@
+% Best First Search
+
+edge(a,b).
+edge(a,c).
+edge(b,d).
+edge(b,e).
+edge(c,f).
+edge(e,g).
+
+best_first(X,Y) :-
+    edge(X,Y).
+
+best_first(X,Y) :-
+    edge(X,Z),
+    best_first(Z,Y).
