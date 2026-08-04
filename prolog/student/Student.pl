f1266bd198f4599fcf5495ac0fcdd1f515ebb943
Shaik roshni <192525185.simats@saveetha.com>
2026-08-04 13:57:48 +0530

    Add files via upload

---
+++ prolog/student/Student.pl
@@ -0,0 +1,24 @@
+% Student Database
+student(roshni).
+student(rahul).
+student(priya).
+
+% Teacher Database
+teacher(arun).
+teacher(suresh).
+
+% Subject Database
+subject(java).
+subject(dbms).
+subject(ai).
+
+% Teacher teaches Subject
+teaches(arun, java).
+teaches(arun, ai).
+teaches(suresh, dbms).
+
+% Student studies Subject
+studies(roshni, java).
+studies(roshni, ai).
+studies(rahul, dbms).
+studies(priya, java).
