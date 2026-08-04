03293bc8b43b1a8ef5f5f42118cd238eb7f1b606
Shaik roshni <192525185.simats@saveetha.com>
2026-08-04 14:08:04 +0530

    Add files via upload

---
+++ prolog/diet/diet.pl
@@ -0,0 +1,7 @@
+% Diet Recommendation System
+
+diet(diabetes, 'Eat vegetables and avoid sugar.').
+diet(bp, 'Reduce salt intake and eat fruits.').
+diet(fever, 'Drink plenty of water and eat light food.').
+diet(obesity, 'Eat low-fat food and exercise regularly.').
+diet(cold, 'Drink warm water and eat vitamin C rich foods.').
