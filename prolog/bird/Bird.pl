4abbbe145e0a20635bb232337d25f46858269b1f
Shaik roshni <192525185.simats@saveetha.com>
2026-08-04 14:03:47 +0530

    Add files via upload

---
+++ prolog/bird/Bird.pl
@@ -0,0 +1,14 @@
+% Bird Database
+
+bird(parrot).
+bird(peacock).
+bird(sparrow).
+bird(ostrich).
+bird(penguin).
+
+can_fly(parrot).
+can_fly(sparrow).
+
+cannot_fly(peacock).
+cannot_fly(ostrich).
+cannot_fly(penguin).
