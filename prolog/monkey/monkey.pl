9c5574f7d0996e80f2e79ee3f84fab359d11b4c0
Shaik roshni <192525185.simats@saveetha.com>
2026-08-04 14:10:01 +0530

    Add files via upload

---
+++ prolog/monkey/monkey.pl
@@ -0,0 +1,8 @@
+% Monkey Banana Problem
+
+can_get_banana :-
+    write('Monkey is near the box.'), nl,
+    write('Monkey pushes the box under the banana.'), nl,
+    write('Monkey climbs on the box.'), nl,
+    write('Monkey reaches the banana.'), nl,
+    write('Monkey gets the banana.'), nl.
